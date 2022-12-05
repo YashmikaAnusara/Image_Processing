b44b95322a207b2bc00e7b915a97685db3d4ca98
Yashmika Anusara <yashmikaaunusara@gmail.com>
2022-12-05 07:27:27 +0530

    bug fix

--- main.py
+++ main.py
@@ -15,23 +15,23 @@ import cv2
 #         break
 
 # Face Detection with camera
-face_para = cv2.CascadeClassifier('Body_Parameters/haarcascade_frontalface_default.xml')
-print("Read Face Parameter...")
-cam = cv2.VideoCapture(0)
-print("Detecting Camera...")
-while True:
-    success, cam_out = cam.read()
-    print("Read the camera...")
-    gray = cv2.cvtColor(cam_out, cv2.COLOR_BGR2GRAY)
-    print("Convert the RGB to Grayscale...")
-    face = face_para.detectMultiScale(gray, 1.1, 4)
-    print("Detecting grayscale image face parameter...")
-    for (x, y, w, h) in face:
-        cv2.rectangle(cam_out, (x, y), (x + w, y + h), (79, 168, 72), 4)
-        cv2.imshow('end_cam', cam_out)
-        if cv2.waitKey(1) & 0xFF == ord('s'):
-            break
-cam.release()
+# face_para = cv2.CascadeClassifier('Body_Parameters/haarcascade_frontalface_default.xml')
+# print("Read Face Parameter...")
+# cam = cv2.VideoCapture(0)
+# print("Detecting Camera...")
+# while True:
+#     success, cam_out = cam.read()
+#     print("Read the camera...")
+#     gray = cv2.cvtColor(cam_out, cv2.COLOR_BGR2GRAY)
+#     print("Convert the RGB to Grayscale...")
+#     face = face_para.detectMultiScale(gray, 1.1, 4)
+#     print("Detecting grayscale image face parameter...")
+#     for (x, y, w, h) in face:
+#         cv2.rectangle(cam_out, (x, y), (x + w, y + h), (79, 168, 72), 4)
+#         cv2.imshow('end_cam', cam_out)
+#         if cv2.waitKey(1) & 0xFF == ord('s'):
+#             break
+# cam.release()
 
 # Face Detection with image
 # face_para = cv2.CascadeClassifier('Body_Parameters/haarcascade_frontalface_default.xml')
